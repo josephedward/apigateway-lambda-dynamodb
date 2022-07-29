3ecafae5f8ceb52907e8b296fa2aae4c1a6841bd
josephedward <josephedwardwork@gmail.com>
2022-07-29 10:13:08 -0400

    Dockerfile per lambda
    - cant pass ECR_REPO as a var to each Dockerfile

--- lambdas/build-python-lambda.sh
+++ lambdas/build-python-lambda.sh
@@ -1,6 +1,6 @@
 aws ecr create-repository --repository-name $ECR_REPOSITORY | echo "repo already created" 
 export ECR_REPOSITORY=$ECR_REPOSITORY
-docker build --build-arg ECR_REPOSITORY=$ECR_REPOSITORY -t $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG . 
+docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG . 
 echo "Pushing image to ECR..."
 docker push $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
 echo "::set-output name=image::$ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG"
