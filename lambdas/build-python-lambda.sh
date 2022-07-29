a361b1ac25d903bf48d5be08569770996555ea04
josephedward <josephedwardwork@gmail.com>
2022-07-29 01:10:34 -0400

    docker

--- lambdas/build-python-lambda.sh
+++ lambdas/build-python-lambda.sh
@@ -1,4 +1,5 @@
 aws ecr create-repository --repository-name $ECR_REPOSITORY | echo "repo already created" 
+export ECR_REPOSITORY=$ECR_REPOSITORY
 docker build --build-arg ECR_REPOSITORY=$ECR_REPOSITORY -t $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG . 
 echo "Pushing image to ECR..."
 docker push $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
